c61b7c04f56049a642cdb3a99e469d993d182071
Eugen <krizhik@gmail.com>
2022-07-17 21:59:12 +0300

    exercise detail

---
+++ src/components/ExerciseVideos.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  console.log('exerciseVideos', exerciseVideos);
+  console.log('name', name);
+  return <div>ExerciseVideos</div>;
+};
+
+export default ExerciseVideos;
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOption, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
-  return <div>ExerciseDetail</div>;
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com/exercises';
+      const youtubeSearchUrl =
+        'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercise/${id}`,
+        exerciseOption
+      );
+      setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
+      <SimilarExercises />
+    </Box>
+  );
 };
 
 export default ExerciseDetail;
